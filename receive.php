27a34d17613b4f6e3040b93ffd9dbbb7bfc9cb55
Jay Bhairaviya <jaybhairaviya1996@gmail.com>
2018-04-08 01:30:52 +0530

    fixing emergency service ambulance

--- receive.php
+++ receive.php
@@ -1,36 +1,36 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/connection.php');
-	session_start();
-	$uid = $_SESSION['uid'];
-	$dr_query = "SELECT dr_id FROM `driver` WHERE uid = $uid LIMIT 1";
-	$dr_result = mysqli_query($conn,$dr_query);
-	$dr_row = mysqli_fetch_array($dr_result);
-	$driver_id = $dr_row['dr_id'];
- ?>
+session_start();
+$uid = $_SESSION['uid'];
+$dr_query = "SELECT dr_id FROM `driver` WHERE uid = $uid LIMIT 1";
+$dr_result = mysqli_query($conn,$dr_query);
+$dr_row = mysqli_fetch_array($dr_result);
+$driver_id = $dr_row['dr_id'];
+?>
 <section id="main-content">
     <section class="wrapper" style="margin-top: 0px;margin-bottom: 0px;">
         <div class="typo-agile">
             <div class="grid_3 grid_4 w3layouts">
                 <h3 class="hdg">Receive | Select Hospital</h3>
                 <div class="bs-example">
-                     <div class="panel panel-default" >
-                        <input id="pac-input" class="controls" type="text" placeholder="SearchBox">
-                        <input type="hidden" name="driver_id" id="dr_id" value="<?php echo $driver_id; ?>">
-                        <a href="" onclick="initAutocomplete();"><i class="fa fa-refresh"></i></a>
+                 <div class="panel panel-default" >
+                    <input id="pac-input" class="controls" type="text" placeholder="SearchBox">
+                    <input type="hidden" name="driver_id" id="dr_id" value="<?php echo $driver_id; ?>">
+                    <a href="" onclick="initAutocomplete();"><i class="fa fa-refresh"></i></a>
 
-                        <br>
-                        <div id="user_info" style="padding-left: 15px;"></div>
-                    </div>
+                    <br>
+                    <div id="user_info" style="padding-left: 15px;"></div>
                 </div>
             </div>
-            <div class="row" style="width: 100%;height: 100%;">
-                
-            </div><!-- /.row -->
         </div>
+        <div class="row" style="width: 100%;height: 100%;">
+
+        </div><!-- /.row -->
+    </div>
 </section>
 <div id="map"></div>
 </section>
-    <script>
+<script>
     // This example adds a search box to a map, using the Google Place Autocomplete
     // feature. People can enter geographical searches. The search box will return a
     // pick list containing a mix of places and predicted search terms.
@@ -41,24 +41,28 @@
     var myLoc;
     var markers = [];
     var infowindow;
+    var enabled = 0;
+    var map;
+    var myMarker;
+
     function initAutocomplete() {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
 
     function showPosition(position) {
         myLoc = { lat: position.coords.latitude, lng: position.coords.longitude };
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
             center: myLoc,
             zoom: 15,
             mapTypeId: 'roadmap'
         });
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-            location: myLoc,
-            radius: 1500,
-            types: ['hospital', 'Hospital']
-        }, callback);
+        // infowindow = new google.maps.InfoWindow();
+        // var service = new google.maps.places.PlacesService(map);
+        // service.nearbySearch({
+        //     location: myLoc,
+        //     radius: 1500,
+        //     types: ['hospital', 'Hospital']
+        // }, callback);
 
         // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
@@ -123,30 +127,45 @@
             }
         }
         function createMarker(place) {
-        // var placeLoc = place.geometry.location;
-        var mark = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-        });
-        google.maps.event.addListener(mark, 'click', function() {
-        	var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>'+
-            '<div id="bodyContent">'+
-            '<a href="http://localhost/EHealth/book.php?hid=' + place.place_id + '">'+
-            'Click here to </a> '+
-            '</p>'+
-            '</div>'+
-            '</div>';
-            infowindow.setContent(contentString);
-            infowindow.open(map, this);
-        });
-        markers.push(mark);
+            // var placeLoc = place.geometry.location;
+            var mark = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            google.maps.event.addListener(mark, 'click', function() {
+            	var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>'+
+                '<div id="bodyContent">'+
+                '<a href="http://localhost/EHealth/book.php?hid=' + place.place_id + '">'+
+                'Click here to </a> '+
+                '</p>'+
+                '</div>'+
+                '</div>';
+                infowindow.setContent(contentString);
+                infowindow.open(map, this);
+            });
+            markers.push(mark);
+        }
+
+        function myMarkerCb(place){
+            console.log(place);
+            console.log('inside');
+            myMarker= new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(place.coords.latitude, place.coords.longitude)
+            });
+        }
+        function createMyMarker(){
+            navigator.geolocation.getCurrentPosition(myMarkerCb);
+        }
+        createMyMarker();
+        enabled = 1;
     }
-    }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvABQiBNANco06dejzBwEQ58Gd2nMP42o&libraries=places&callback=initAutocomplete" async defer></script>
+
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvABQiBNANco06dejzBwEQ58Gd2nMP42o&libraries=places&callback=initAutocomplete" async defer></script>
 <script src="js/receive_ajax.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/jquery.dcjqaccordion.2.7.js"></script>
