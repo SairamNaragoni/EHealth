71a9fa7fb469ee1ce8d81151d5289b8921848fd6
Sairam <naragonisairam@gmail.com>
2018-04-08 14:11:15 +0530

    unwanted images deleted

--- hospital/includes/pastAppointments.php
+++ hospital/includes/pastAppointments.php
@@ -20,7 +20,6 @@
                             <th>Reason</th>
                             <th>Customer No</th>
                             <th>Date</th>
-                            <th>Time</th>
                             <th>View Report</th>
                     <tbody>
                             <?php  
@@ -38,8 +37,38 @@
                                     <td class="view-message dont-show" ><?php echo $row['reason']?></td>
                                     <td class="view-message dont-show" ><?php echo $row['phone']?></td>
                                     <td class="view-message dont-show" ><?php echo $row['date']?></td>
-                                    <td class="view-message dont-show" ><?php echo $row['approved_time']?></td>
-                                    <td class="view-message view-message"><a href="report.php?a_id=<?php echo $row['aid']?>" id="approve">View Report</a></td>
+                                    <td> <a href="#myModal" data-toggle="modal" class="btn btn-success">View Report</a></td>
+                                                <?php 
+                                                    $aidhere = $row['aid'];
+                                                    $report_query = "SELECT * FROM `report` WHERE aid = $aidhere";
+                                                    $report_result = mysqli_query($conn,$report_query);
+                                                    $report_row = mysqli_fetch_array($report_result);
+                                                 ?>
+                                            <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                                                        <h4 class="modal-title">Report</h4>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <form role="form" method="post" enctype="multipart/form-data" class="uform">
+                                                            <div class="form-group">
+                                                                <label for="summary">Summary</label>
+                                                                 <textarea class="form-control summary" rows="3" name="summary" required=""><?php echo $report_row['summary']; ?></textarea>
+                                                            </div>
+                                                            <div class="form-group">
+                                                               <label for="prescription">Prescription</label>
+                                                                 <textarea class="form-control prescription" rows="3" name="prescription" required=""><?php echo $report_row['prescription']; ?></textarea>
+                                                            </div>
+                                                            <div>
+                                                                <a href="../images/reports/<?php echo $report_row['report']; ?>" data-toggle="modal" class="btn btn-success" target = "_blank">View File </a></td>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </div></td>
                                 </tr>    
                             <?php }}
                             else { ?>
