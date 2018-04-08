82581c045ef5b7ae5af2f80afd0a6ea8d8a6d56a
Sairam <naragonisairam@gmail.com>
2018-04-08 11:06:31 +0530

    changed dashboard

--- home.php
+++ home.php
@@ -5,48 +5,24 @@
 	<section class="wrapper">
 		<div class="agil-info-calendar">
 		<!-- calendar -->
-		<div class="col-md-6 agile-calendar">
-			<div class="calendar-widget">
-                <div class="panel-heading ui-sortable-handle">
-					<span class="panel-icon">
-                      <i class="fa fa-calendar-o"></i>
-                    </span>
-                    <span class="panel-title"> Calendar Widget</span>
-                </div>
-				<!-- grids -->
-					<div class="agile-calendar-grid">
-						<div class="page">
-							
-							<div class="w3l-calendar-left">
-								<div class="calendar-heading">
-									
-								</div>
-								<div class="monthly" id="mycalendar"></div>
-							</div>
-							
-							<div class="clearfix"> </div>
-						</div>
-					</div>
-			</div>
+		<div class="col-md-3 agile-calendar">
 		</div>
 		<!-- //calendar -->
 		<div class="col-md-6 w3agile-notifications">
 			<div class="notifications">
 				<!--notification start-->
-				
 					<header class="panel-heading">
-						Notification 
+						DASHBOARD	 
 					</header>
 					<div class="notify-w3ls">
 						<div class="alert alert-info clearfix">
 							<span class="alert-icon"><i class="fa fa-envelope-o"></i></span>
 							<div class="notification-info">
 								<ul class="clearfix notification-meta">
-									<li class="pull-left notification-sender"><span><a href="#">Jonathan Smith</a></span> send you a mail </li>
-									<li class="pull-right notification-time">1 min ago</li>
+									<li class="pull-left notification-sender"><span><a href="#">Booking Appoinments</a></span> is now at your doorstep. </li>
 								</ul>
 								<p>
-									Urgent meeting for next proposal
+									Select Book Appointments -> Hospital-> Doctor -> Date -> Book
 								</p>
 							</div>
 						</div>
@@ -54,11 +30,10 @@
 							<span class="alert-icon"><i class="fa fa-facebook"></i></span>
 							<div class="notification-info">
 								<ul class="clearfix notification-meta">
-									<li class="pull-left notification-sender"><span><a href="#">Jonathan Smith</a></span> mentioned you in a post </li>
-									<li class="pull-right notification-time">7 Hours Ago</li>
+									<li class="pull-left notification-sender"><span><a href="#">Emergency Services</a></span> are now faster than ever </li>
 								</ul>
 								<p>
-									Very cool photo jack
+									Select Emergency Services -> Get An Ambulance
 								</p>
 							</div>
 						</div>
@@ -66,11 +41,10 @@
 							<span class="alert-icon"><i class="fa fa-comments-o"></i></span>
 							<div class="notification-info">
 								<ul class="clearfix notification-meta">
-									<li class="pull-left notification-sender">You have 5 message unread</li>
-									<li class="pull-right notification-time">1 min ago</li>
+									<li class="pull-left notification-sender"><span><a href="#">Blood Requests</a></span> are now efficient and easier </li>
 								</ul>
 								<p>
-									<a href="#">Anjelina Mewlo, Jack Flip</a> and <a href="#">3 others</a>
+									Login From Hospital Dashboard -> Blood Drive
 								</p>
 							</div>
 						</div>
@@ -78,11 +52,10 @@
 							<span class="alert-icon"><i class="fa fa-bell-o"></i></span>
 							<div class="notification-info">
 								<ul class="clearfix notification-meta">
-									<li class="pull-left notification-sender">Domain Renew Deadline 7 days ahead</li>
-									<li class="pull-right notification-time">5 Days Ago</li>
+									<li class="pull-left notification-sender"><span><a href="#">Medical Reports</a></span> are now automated </li>
 								</ul>
 								<p>
-									Next 5 July Thursday is the last day
+									The Doctor uploads the reports
 								</p>
 							</div>
 						</div>
@@ -90,11 +63,10 @@
 							<span class="alert-icon"><i class="fa fa-envelope-o"></i></span>
 							<div class="notification-info">
 								<ul class="clearfix notification-meta">
-									<li class="pull-left notification-sender"><span><a href="#">Jonathan Smith</a></span> send you a mail </li>
-									<li class="pull-right notification-time">1 min ago</li>
+									<li class="pull-left notification-sender"><span><a href="#">Hospital Events</a></span> are notifed </li>
 								</ul>
 								<p>
-									Urgent meeting for next proposal
+									The Hospital updates the events details
 								</p>
 							</div>
 						</div>
@@ -124,45 +96,6 @@
 <script src="js/jquery.nicescroll.js"></script>
 <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
 <script src="js/jquery.scrollTo.js"></script>
-<!-- morris JavaScript -->	
-<script>
-	$(document).ready(function() {
-		//BOX BUTTON SHOW AND CLOSE
-	   jQuery('.small-graph-box').hover(function() {
-		  jQuery(this).find('.box-button').fadeIn('fast');
-	   }, function() {
-		  jQuery(this).find('.box-button').fadeOut('fast');
-	   });
-	   jQuery('.small-graph-box .box-close').click(function() {
-		  jQuery(this).closest('.small-graph-box').fadeOut(200);
-		  return false;
-	   });
-	   
-	    //CHARTS
-	    function gd(year, day, month) {
-			return new Date(year, month - 1, day).getTime();
-		}
-	});
-	</script>
-<!-- calendar -->
-	<script type="text/javascript" src="js/monthly.js"></script>
-	<script type="text/javascript">
-		$(window).load( function() {
-			$('#mycalendar').monthly({
-				mode: 'event',
-				
-			});
-			$('#mycalendar2').monthly({
-				mode: 'picker',
-				target: '#mytarget',
-				setWidth: '250px',
-				startHidden: true,
-				showTrigger: '#mytarget',
-				stylePast: true,
-				disablePast: true
-			});
-		});
-	</script>
-	<!-- //calendar -->
+
 </body>
 </html>
